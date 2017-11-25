2a335b8b339000232713d100bf793d1945e2f433
felix <felixenchiparambil@hotmail.com>
2017-11-25 17:11:48 +0100

    init data access layer setup

--- AutoReservation.Dal/AutoReservationContext.cs
+++ AutoReservation.Dal/AutoReservationContext.cs
@@ -6,6 +6,10 @@ namespace AutoReservation.Dal
 {
     public class AutoReservationContext : DbContext
     {
+        public DbSet<Auto> Autos { get; set; }
+        public DbSet<Kunde> Kunden { get; set; }
+        public DbSet<Reservation> Reservationen { get; set; }
+
         public AutoReservationContext()
         {
             // Ensures that the database will be initialized
@@ -41,6 +45,7 @@ namespace AutoReservation.Dal
             //      Remarks:
             //      This could not be done using attributes on business entities
             //      since the discriminator (AutoKlasse) must not be part of the entity.
+
         }
     }
 }
